39ecc9d1210f15e71f39c20444d1879214ae37a7
Eric Chu <eytchu@gmail.com>
2013-09-18 16:29:40 -0700

    started pardiso branch

--- run_pdos.c
+++ run_pdos.c
@@ -4,6 +4,10 @@
 #ifndef DEMO_PATH
 #define DEMO_PATH "../data/portfolio_test_01_10x100"
 #endif
+<<<<<<< HEAD
+=======
+
+>>>>>>> started pardiso branch
 static timer PDOS_timer;
 
 int main(int argc, char **argv)
@@ -35,6 +39,7 @@ void read_in_data(FILE * fp, Data ** d, Cone ** k){
 	fread(&((*k)->f), sizeof(idxint), 1, fp);
 	fread(&((*k)->l), sizeof(idxint), 1, fp);
 	fread(&((*k)->qsize), sizeof(idxint), 1, fp);
+<<<<<<< HEAD
 
   (*k)->q = malloc(sizeof(idxint)*(*k)->qsize);
   fread((*k)->q, sizeof(idxint), (*k)->qsize, fp);
@@ -45,6 +50,18 @@ void read_in_data(FILE * fp, Data ** d, Cone ** k){
   (*d)->c = malloc(sizeof(double)*(*d)->n);
   fread((*d)->c, sizeof(double), (*d)->n, fp);
 
+=======
+  
+  (*k)->q = malloc(sizeof(idxint)*(*k)->qsize);
+  fread((*k)->q, sizeof(idxint), (*k)->qsize, fp);
+  
+  (*d)->b = malloc(sizeof(double)*(*d)->m);
+  fread((*d)->b, sizeof(double), (*d)->m, fp);
+  
+  (*d)->c = malloc(sizeof(double)*(*d)->n);
+  fread((*d)->c, sizeof(double), (*d)->n, fp);
+  
+>>>>>>> started pardiso branch
   fread(&((*d)->p->MAX_ITERS), sizeof(idxint), 1, fp);
   fread(&((*d)->p->CG_MAX_ITS), sizeof(idxint), 1, fp);
   fread(&((*d)->p->ALPHA), sizeof(double), 1, fp);
@@ -67,8 +84,6 @@ void read_in_data(FILE * fp, Data ** d, Cone ** k){
   fread((*d)->Ax, sizeof(double), Anz, fp);
   
   (*d)->x = NULL; (*d)->y = NULL; (*d)->s = NULL;
-
-
 
   /*
   fscanf(fp, "%zu", &NNZ);
